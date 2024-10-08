9cf68997ecd80923d6a6aa3bf7ba225af7a130c7
guilhermefsz <franciscoguilherme028@gmail.com>
2024-10-08 10:33:20 -0400

    testando

--- App/Libraries/Database.php
+++ App/Libraries/Database.php
@@ -4,31 +4,38 @@ class Database{
     private $usuario = "root";
     private $senha = "";
     private $banco = "blogaula";
-    private $porta = "3306";
+    private $porta = "3306"; //verificar a porta do seu banco
     private $dbh;
     private $stmt;
 
     public function __construct(){
-         
-        $dns = 'mysql:host='.$this->host.';port='.$this->porta.';dbname'.$this->banco;
+        //fonte de dados ou DNS  que contém as informações para conectar ao banco de dados.
+        $dns = 'mysql:host='.$this->host.';port='.$this->porta.';dbname='.$this->banco;
+        
         $opcoes = [
+             //armanezar em cache a conexão para ser reutilizada, evitando sobrecarga de uma nova conexão. 
             PDO::ATTR_PERSISTENT => true,
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            //lança um PDOException se ocorrer um erro
+            PDO::ATTR_ERRMODE=> PDO::ERRMODE_EXCEPTION
         ];
         try{
+            //cria a instacia do PDO
             $this->dbh = new PDO($dns, $this->usuario, $this->senha, $opcoes);
-        }
-        catch(PDOException $error){
-            print "Error!";$error->getMessage()."<br/>";
-            die();
-        }
-       
-    }//fim do método constrututor
+        }catch(PDOException $error){
+          print "Error!";$error->getMessage()."<br/>";
+          die();
+          //fala
+        }//fim do catch
+    }//fim do método construtor
+
+    // prepara o statement com query
     public function query($sql){
+        //prepara a consulta sql
         $this->stmt= $this->dbh->prepare($sql);
-    }
+    }//fim da função query
+    //vincula um valor a um parâmetro
     public function bind($parametro, $valor, $tipo= null){
-        if (is_null($tipo)):
+        if(is_null($tipo)):
             switch(true):
                 case is_int($valor):
                     $tipo = PDO::PARAM_INT;
@@ -41,31 +48,32 @@ class Database{
                     break;
                 default:
                     $tipo = PDO::PARAM_STR;
-                endswitch;
-            endif;
-            $this->stmt->bindValue($parametro, $valor, $tipo);
-         }
-         public function executa(){
-            return $this ->stmt->execute();
-         }//fim da função executa
+            endswitch;
+        endif;
+        $this->stmt->bindValue($parametro, $valor, $tipo);
+    }//fim da função bind
+    //executa prepared statement
+     
+    public function executa(){
+        return $this->stmt->execute();
+    }//fim da função executa
 
-         public function resultado(){
-            $this->executa();
-            return $this->stmt->fetch(PDO::FETCH_OBJ);
-         }//fim da função resultado
-
-         public function resultados(){
-            $this->executa();
-            return $this->stmt->fetchAll(PDO::FETCH_OBJ);
-         }//fim da função resultados
-
-         public function totalResultado(){
-            return $this->stmt->rowCount();
-         }// fim da função totalResultados
-
-         public function ultimoIdInserido(){
-            return $this->dbh->lastInsertId();
-         }//fim da função ultimoIdInserido
-    }
-        
-          
+    //obtem um único registro 
+    public function resultado(){
+        $this->executa();
+        return $this->stmt->fetch(PDO::FETCH_OBJ);
+    }//fim da função resultados
+    //obtem um conjunto de registros
+    public function resultados(){
+        $this->executa();
+        return $this->stmt->fetchAll(PDO::FETCH_OBJ);
+    }//fim da função resultados
+    //retorna o número de linhas afetadas pela última instrução SQL
+    public function totalResultados(){
+        return $this->stmt->rowCount();
+    }//fim da função totalResultados
+    //retorna o ultimo ID inserido no banco de dados
+    public function ultimoIdInserido(){
+        return $this->dbh->lastInsertId();
+    }//fim da função ultimoIdInserido
+}//fim da classe Database
--- Public/index.php
+++ Public/index.php
@@ -3,7 +3,63 @@ include "./../App/configuracao.php";
 include "./../App/Libraries/Rota.php";
 include "./../App/Libraries/Controller.php";
 include "./../App/Libraries/Database.php";
+
+$db = new Database;
+$db->query("SELECT * FROM posts");
+foreach($db->resultados() as $post){
+    echo $post->titulo. ' | '.$post->texto.'<br>';
+}
+/*
+$db->query("SELECT * FROM posts ORDER BY id DESC");
+$db->resultado();
+echo $db->resultado()->titulo;
+*/
+
+/*
+$id = 1;
+$db->query("DELETE FROM posts WHERE id = :id");
+$db->bind(":id", $id);
+$db->executa();
+echo '<hr> Total Atualizado: '.$db->totalResultados();
+*/
+
+/*
+date_default_timezone_set('America/Cuiaba');
+$id = 2;
+$usuario_id = 8;
+$titulo = "Algoritmos";
+$texto = "Algoritmos é muito legal";
+$criadoEm = date('Y-m-d H:i:s');
+
+$db->query("UPDATE posts SET usuario_id = :usuario_id, titulo = :titulo, texto = :texto, criado_em=:criadoEm WHERE id =:id");
+
+$db->bind(":id",$id);
+$db->bind(":usuario_id",$usuario_id);
+$db->bind(":titulo",$titulo);
+$db->bind(":texto",$texto);
+$db->bind(":criadoEm",$criadoEm);
+
+$db->executa();
+echo '<hr>Total Resultados: '.$db->totalResultados();
+*/
+
+/*
+$usuario_id = 8;
+$titulo = "Programação";
+$texto = "Programação Web";
+
+$db->query("INSERT INTO posts (usuario_id, titulo, texto) VALUES (:usuario_id, :titulo, :texto)");
+$db->bind(":usuario_id", $usuario_id);
+$db->bind(":titulo", $titulo);
+$db->bind(":texto", $texto);
+
+$db->executa();
+
+echo '<hr>Total Resultados: '.$db->totalResultados();
+echo '<hr>Ultimo ID inserido'.$db->ultimoIdInserido();
+*/
 ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
