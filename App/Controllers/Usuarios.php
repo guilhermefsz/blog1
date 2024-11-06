04fa2b388f11c01a8d8d15ca0cb5000966de084d
guilhermefsz <franciscoguilherme028@gmail.com>
2024-11-06 10:33:39 -0400

    model 06.11.2024

--- App/Controllers/Usuarios.php
+++ App/Controllers/Usuarios.php
@@ -1,12 +1,84 @@
 <?php
 
 class Usuarios extends Controller{
-    public function cadastrar(){
-        $formulario = filter_input_array(
-            INPUT_POST, ['nome' =>FILTER_SANITIZE_FULL_SPECIAL_CHARS,  'email' =>FILTER_SANITIZE_EMAIL,
-             'senha' =>FILTER_DEFAULT,  'confirmar_senha' =>FILTER_DEFAULT]);
 
+    public function __construct(){
+        $this->$usuarioModel = $this->model('Usuario');
+    }
+
+    public
         
-        $this->view('usuarios/cadastrar');
+    
+
+
+    public function cadastrar()
+    {
+
+        $formulario = filter_input_array(INPUT_POST, FILTER_SANITIZE_SPECIAL_CHARS);
+        if (isset($formulario)) :
+            $dados = [
+                'nome' => trim($formulario['nome']),
+                'email' => trim($formulario['email']),
+                'senha' => trim($formulario['senha']),
+                'confirma_senha' => trim($formulario['confirma_senha']),
+            ];
+
+            if (in_array("", $formulario)) :
+
+                if (empty($formulario['nome'])) :
+                    $dados['nome_erro'] = 'Preencha o campo nome';
+                endif;
+
+                if (empty($formulario['email'])) :
+                    $dados['email_erro'] = 'Preencha o campo e-mail';
+                endif;
+
+                if (empty($formulario['senha'])) :
+                    $dados['senha_erro'] = 'Preencha o campo senha';
+                endif;
+
+                if (empty($formulario['confirma_senha'])) :
+                    $dados['confirma_senha_erro'] = 'Confirme a Senha';
+                endif;
+            else :
+                if(Checa::checarNome($formulario['nome'])):
+                    $dados['nome_erro'] = 'O nome informado é inválido';
+                    elseif(Checa::checarEmail($formulario['email'])):
+                        $dados['email_erro'] = 'O email informado é inválido';
+
+                elseif (strlen($formulario['senha']) < 6) :
+                    $dados['senha_erro'] = 'A senha deve ter no minimo 6 caracteres';
+                elseif ($formulario['senha'] != $formulario['confirma_senha']) :
+                    $dados['confirma_senha_erro'] = 'As senhas são diferentes';
+                else:
+                    $dados['senha'] = password_hash($formulario['senha'], PASSWORD_DEFAULT);
+                    if($this->usuarioModel->armazenar($dados)):
+                    echo 'Cadastro realizado com sucesso <hr>';
+                    else:
+                        die("Error ao armazenar o usúario no banco de dados");
+                    endif;
+                endif;
+
+
+            endif;
+            /*
+            echo'senha original: '.$formulario['senha']."<hr>";
+            echo'senha MD5: '.md5($formulario['senha'])."<hr>";
+            echo '<hr>';
+            $senha_segura = password_hash($formulario['senha'],PASSWORD_DEFAULT);
+            echo'senha Hash: '.$senha_segura.'<hr>';
+            */
+
+            var_dump($formulario);
+        else :
+            $dados = [
+                'nome' => '',
+                'email' => '',
+                'senha' => '',
+                'confirma_senha' => '',
+            ];
+        endif;
+
+        $this->view('usuarios/cadastrar', $dados);
     }
 }
--- App/Views/usuarios/cadastrar.php
+++ App/Views/usuarios/cadastrar.php
@@ -1,33 +1,40 @@
 <div class="col-xl-4 col-md-6 mx-auto p-5">
-    <div  class="card">
-        <div class="card-header">
-        Cadastre-se
+    <div class="cartão">
+        <div class="cabeçalho-do-cartão">
+            Cadastre-se
         </div>
-        <div class="card-body">
-            <p class="card-text"><small class="text-muted">
-                Preencha o formulário abaixo para fazer o seu cadastro
-            </small></p>
-            <form name="cadastrar" method="POST" action="<?=URL?>/usuarios/cadastrar" class="mt-4">
-                <div class="form-group">
+        <div class="corpo-do-cartão">
+            <p class="card-text"><small class="text-muted">Preencha o formulário abaixo para fazer seu cadastro</small></p>
+
+            <form nome="cadastrar" método="POST"
+             action="<?=URL?>/usuarios/cadastrar" class="mt-4">
+                <div class="grupo-de-formulários">
                     <label for="nome">Nome: <sup class="text-danger">*</sup></label>
-                    <input type="text" name="nome" id="nome" class="form-control" requice>
+                    <tipo de entrada="texto" nome="nome" id="nome" classe="controle-de-formulário" require 
+                    value="<?=$dados['nome']?>" class='form-control<?=$dados['nome_erro'] ? 'is-invalid': '' ?>'>
+                    <div class="invalid-feedback"><?=$dados['nome_erro']?></div>
                 </div>
-                <div class="form-group">
-                    <label for="nome">Email: <sup class="text-danger">*</sup></label>
-                    <input type="email" name="email" id="email" class="form-control" requice>
+
+                <div class="grupo-de-formulários">
+                    <label for="email">E-mail: <sup class="text-danger">*</sup></label>
+                    <input tipo="email" nome="email" id="email" classe="form-control" require>
                 </div>
-                <div class="form-group">
-                    <label for="nome">Senha: <sup class="text-danger">*</sup></label>
-                    <input type="password" name="senha" id="senha" class="form-control" requice>
+
+                <div class="grupo-de-formulários">
+                    <label for="senha">Senha: <sup class="text-danger">*</sup></label>
+                    <input type="senha" nome="senha" id="senha" class="form-control" require>
                 </div>
-                <div class="form-group">
-                    <label for="nome">Confirma Senha: <sup class="text-danger">*</sup></label>
-                    <input type="password" name="confirma-senha" id="confirma-senha" class="form-control" requice>
+
+                <div class="grupo-de-formulários">
+                    <label for="confirma_senha">Confirma Senha: <sup class="text-danger">*</sup></label>
+                    <input type="senha" name="confirma_senha" id="confirma_senha" class="form-control" require>
                 </div>
-                <div class="row">
+
+                <div class="linha">
                     <div class="col">
-                        <input type="submit" value="Cadastrar" class="btn btn-info btn-block">
+                        <input type="submit" value="Cadastrar"class="btn btn-info btn-block">
                     </div>
+
                     <div class="col">
                         <a href="#">Você tem uma conta? Faça login</a>
                     </div>
